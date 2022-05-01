b08b795975cd54617953cf2b3415a1c595ef5901
JK <jrileykruger@gmail.com>
2022-04-30 21:55:51 -0700

    added insert queries, partially completed update employee role

--- db/queries.sql
+++ db/queries.sql
@@ -14,3 +14,22 @@ JOIN role ON role.department_id = department.id
 JOIN employee ON employee.role_id = role.id
 LEFT JOIN employee e ON e.id = employee.manager_id;
 
+-- Add Department
+INSERT INTO department (name)
+VALUES ("Service");
+
+-- Add Role
+INSERT INTO role (title, salary, department_id)
+VALUES ("Customer Service", 80000, 5);      -- Customer Service | 80000 | Service
+
+-- Add Employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sam", "Kash", 9, 3);    -- Sam | Kash | Customer Service | Ashley Rodriguez
+
+-- TODO
+-- Update Employee Role
+    -- Search Employees by concatenated name
+    -- Search Roles
+UPDATE employee
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
